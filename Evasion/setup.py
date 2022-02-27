54205463f7134c4894de75cbfe3b711421033e7f
Cyb3rDud3 <cyb3rguy1337@gmail.com>
2022-02-27 20:05:33 +0200

    cythonized Evasion.utils

--- Evasion/setup.py
+++ Evasion/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("backup/debugEvasion.pyx", language_level = "3str")
+    ext_modules = cythonize("backup/utils.pyx", language_level = "3str")
 )
