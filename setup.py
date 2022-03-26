c7603882fa2d0f34586fc33095270ee4e3d166da
Cyb3rDud3 <cyb3rguy1337@gmail.com>
2022-03-26 10:36:22 +0300

    changed structure of folders --> no more backup folders

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import Cython.Compiler.Options
 Cython.Compiler.Options.docstrings = False
 Cython.Compiler.Options.emit_code_comments = True
 
+
 setup(
-    ext_modules = cythonize("backup/helpers.pyx", language_level = "3str",)
+    ext_modules = cythonize("cython_sources/*.pyx", language_level = "3str",)
 )
