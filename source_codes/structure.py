73531210ef7cb92b628cb9fd7f382e82eda2ed91
Cyb3rDud3 <cyb3rguy1337@gmail.com>
2022-04-30 18:12:09 +0300

    .

--- source_codes/structure.py
+++ source_codes/structure.py
@@ -1,16 +1,17 @@
+import shutil
 from zipfile import ZipFile
 import os
-from Utils.helpers import random_string
-from obfuscation.main import Obfuscator,ValidateGlobalVars,parse,unparse
-def zip_dir(base_dir: str,zip_name: str,zip_password: str) -> str:
-    zf = ZipFile(zip_name,"a")
-    for dirname, subdirs, files in os.walk(base_dir):
-        zf.write(dirname)
-        for filename in files:
-            zf.write(os.path.join(dirname, filename))
-    zf.setpassword(zip_password.encode())
-    return os.path.abspath(os.path.join(os.getcwd(),zip_name))
 
+import pyminizip
+
+from Utils.helpers import random_string
+from pyminizip import compress_multiple
+
+def get_zip_roots(base_folder):
+    roots = []
+    for root,dirs,files in os.walk(base_folder):
+        roots.append(root)
+    return roots
 
 class ArchiveHandler:
     def __init__(self, archive_file,password,base_dir=os.getcwd()):
@@ -18,8 +19,6 @@ class ArchiveHandler:
         self.password = password
         self.base_dir = base_dir
         self.random_folder_name = random_string(is_random=True)
-        self.obfuscation_tree: dict = {}
-        self.obfuscator = Obfuscator()
 
 
     def is_already_unpacked(self):
@@ -34,60 +33,39 @@ class ArchiveHandler:
             file.extractall(path=self.random_folder_name,pwd=self.password)
         return os.path.abspath(os.path.join(os.getcwd(),self.random_folder_name))
 
-    def pack(self,source_path,dst_name,dst_password):
-        return zip_dir(base_dir=source_path,zip_name=dst_name,zip_password=dst_password)
+    def generate_zip_password(self):
+        if os.path.exists('zip_password.py'):
+            os.remove('zip_password.py')
+        with open('zip_password.py','w') as new_password_file:
+            random_password = random_string(is_random=True)
+            declare_password = f"PASSWORD = '{random_password}'"
+            new_password_file.write(declare_password)
+            return random_password
+
+    def locate_zip_password(self):
+        from .zip_password import PASSWORD
+        if not PASSWORD:
+            return self.generate_zip_password()
+        return PASSWORD
+
+    def overwrite_previous_zip(self,zip_path):
+        if os.path.exists("../sources.zip"):
+            os.remove("../sources.zip")
+        shutil.copyfile(zip_path,"../sources.zip")
 
 
-    def add_file_to_tree(self,file_name,code):
-        self.obfuscation_tree[file_name] = code
+    def pack(self,source_path: str,dst_name: str):
+        if not dst_name.endswith('.zip'):
+            dst_name += ".zip"
+        dst_password = self.locate_zip_password()
+        file_list = get_zip_roots(source_path)
+        pyminizip.compress_multiple(file_list,dst_name,dst_password,9)
+        self.overwrite_previous_zip(dst_name)
 
-    def update_filename_in_tree(self,prev_filename,new_filename):
-        if self.get_file_from_tree(prev_filename):
-            self.obfuscation_tree[new_filename] = self.obfuscation_tree[prev_filename]
-            del self.obfuscation_tree[prev_filename]
+"""
+Flow ==> 
+1. python handle && pyinstaller obfuscation
+2. try to unpack the sources.zip with password from .zip_password file
+3. compile the unpacked content into single binary
 
-    def get_file_from_tree(self,file_name):
-        return self.obfuscation_tree.get(file_name)
-
-
-    def obfuscate_filenames(self,base_path):
-        for root,dirs,files in os.walk(base_path):
-            for __dir in dirs:
-                for file in os.listdir(os.path.join(root,__dir)):
-                    full_path = os.path.join(file,os.path.join(root,__dir))
-                    if self.obfuscation_tree.get(full_path):
-                        self.update_filename_in_tree(prev_filename=random_string(is_random=True,is_py=True))
-        for file in os.listdir(base_path):
-            full_path = os.path.join(base_path,file)
-            if self.obfuscation_tree.get(full_path):
-                self.update_filename_in_tree(prev_filename=random_string(is_random=True, is_py=True))
-
-
-    def parse_module_first_time(self,module_path):
-        for root, dirs, files in os.walk(module_path):
-            for __dir in dirs:
-                for file in os.listdir(os.path.join(root, __dir)):
-                    full_path = os.path.join(file, os.path.join(root, __dir))
-                    if os.path.isfile(full_path) and full_path.endswith('.py'):
-                        with open(full_path, 'r') as py_file:
-                            self.add_file_to_tree(file_name=full_path, code=py_file.read())
-        for file in os.listdir(module_path):
-            if os.path.isdir(file) and file.endswith('.py'):
-                with open(os.path.join(module_path, file), 'r') as py_file:
-                    self.add_file_to_tree(file_name=os.path.join(module_path, file), code=py_file.read())
-
-    def obfuscate(self):
-        for full_filename,full_code in self.obfuscation_tree.items():
-            tree = parse(full_code)
-            root = self.obfuscator.visit(tree)
-            root = ValidateGlobalVars(self.obfuscator.global_variables,self.obfuscator.imports).visit(root)
-            self.obfuscation_tree[full_filename] = unparse(root)
-
-    def unpack_and_obfuscate(self):
-        if self.is_already_unpacked():
-            return os.path.join(self.base_dir,self.random_folder_name)
-        unpacked_path = self.unpack()
-        self.parse_module_first_time(unpacked_path)
-        self.obfuscate_filenames(unpacked_path)
-
-
+"""
